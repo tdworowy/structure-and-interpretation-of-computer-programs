0a0ff6b235a318437f20997ac2ec71e508e621bc
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-09-14 11:45:46 +0200

    update

---
+++ basic/square-root2.clj
@@ -0,0 +1,26 @@
+;Square Roots via Newton's Method
+
+(defn square [x]
+  (* x x))
+
+(defn average [x y]
+  (/ (+ x y) 2))
+
+(defn sqrt [x]
+  (defn good-enough? [guess]
+    (< (abs (- (square guess) x)) 0.001))
+  (defn improve [guess]
+    (average guess (/ x guess)))
+
+  (defn sqrt-iter [guess]
+    (if (good-enough? guess) guess
+        (sqrt-iter (improve guess))))
+
+  (sqrt-iter 1.0))
+
+(println (sqrt 2))
+(println (sqrt 9))
+(println (sqrt 99))
+
+(println (sqrt 814728764))
+(println (sqrt 0.004))
