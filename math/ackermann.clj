6f3300590862cf266813d6cb29af91041afcb1ab
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-09-21 11:39:33 +0200

    update

---
+++ math/ackermann.clj
@@ -0,0 +1,36 @@
+(defn A0 [x y]
+  (cond
+    (= y 0) 0
+    (= x 0) (* 2 y)
+    (= y 1) 2
+    :else (A0 (- x 1) (A0 x (- y 1))))) ; from book
+
+(defn A [x y]
+  (cond
+    (= x 0) (+ y 1)
+    (= y 0) (A (- x 1) 1)
+    :else (A (- x 1) (A x (- y 1)))))
+
+(println (A 1 10)) ; 12
+(println (A 2 4)) ; 11
+(println (A 3 3)) ; 61
+
+(println (A 0 0)) ; 1
+(println (A 1 0)) ; 2
+(println (A 0 1)) ; 2
+
+(println (A0 0 1))
+(println (A0 1 1))
+(println (A0 2 1))
+
+(println (A0 0 2))
+(println (A0 1 2))
+(println (A0 2 2))
+
+(println (A0 0 3))
+(println (A0 1 3))
+(println (A0 2 3))
+
+(println (A0 0 4))
+(println (A0 1 4))
+(println (A0 2 4))
---
+++ math/fibonacci.clj
@@ -0,0 +1,11 @@
+(defn fib-iter [a b count]
+  (if (= count 0)
+    b
+    (fib-iter (+ a b) a (- count 1))))
+
+(defn fib [n]
+  (fib-iter 1 0 n))
+
+(println (fib 4))
+(println (fib 12))
+(println (fib 53))
