fdea134b211151f86047add27081f950b6a57a1a
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-11-01 11:22:10 +0100

    update

--- src/structure_and_interpretation_of_computer_programs/exercises/simpson_rule.clj
+++ src/structure_and_interpretation_of_computer_programs/exercises/simpson_rule.clj
@@ -1,5 +1,3 @@
-
-
 (defn sum [term a next b]
   (if (> a b) 0
       (+ (term a) (sum term (next a) next b)))) ;linear recursion
--- src/structure_and_interpretation_of_computer_programs/math/half_interval.clj
+++ src/structure_and_interpretation_of_computer_programs/math/half_interval.clj
@@ -1,16 +1,18 @@
+(require '[clojure.math :as math])
+
 (defn average [numbers] (/ (apply + numbers) (count numbers)))
 (defn close-enough? [x y]
   (< (abs (- x y)) 0.001))
 
 (defn search [f neg-point pos-point]
-  (let [mindpoint (average [neg-point pos-point])]
+  (let [midpoint (average [neg-point pos-point])]
     (if (close-enough? neg-point pos-point)
-      mindpoint
-      (let [test-value (f mindpoint)]
+      midpoint
+      (let [test-value (f midpoint)]
         (cond
-          (pos? test-value) (search f neg-point mindpoint)
-          (neg? test-value) (search f mindpoint pos-point)
-          :else mindpoint)))))
+          (pos? test-value) (search f neg-point midpoint)
+          (neg? test-value) (search f midpoint pos-point)
+          :else midpoint)))))
 
 (defn half-interval-method [f a b]
   (let [a-value (f a) b-value (f b)]
@@ -18,3 +20,5 @@
       (and (neg? a-value) (pos? b-value)) (search f a b)
       (and (neg? b-value) (pos? a-value)) (search f b a)
       :else (throw  (Throwable. "values are not of opposite sign")))))
+
+(println (half-interval-method math/sin 2.0 4.0))
