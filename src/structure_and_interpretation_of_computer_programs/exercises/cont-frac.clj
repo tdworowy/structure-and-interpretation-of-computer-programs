5b382ec6d5419562f5b331e600026efb987fd08a
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-11-08 11:26:16 +0100

    update

---
+++ src/structure_and_interpretation_of_computer_programs/exercises/cont-frac.clj
@@ -0,0 +1,44 @@
+(require '[clojure.math :as math])
+
+(defn cont-frac-recur [n d k]
+  (letfn [(recur-fn [i]
+            (if (= k i) (/ (n i) (d i))
+                (/ (n i) (+ (d i) (recur-fn (+ 1 i))))))]
+    (recur-fn 1)))
+
+(defn cont-frac-iter [n d k]
+  (letfn [(iter-fn [i result]
+            (if (= 0 i) result
+                (iter-fn (- i 1) (/ (n i) (+ result (d i))))))]
+
+    (iter-fn (- k 1) (/ (n k) (d k)))))
+
+(println (cont-frac-recur (fn [i] 1.0)
+                          (fn [i] 1.0)
+                          100))
+
+(println (cont-frac-iter (fn [i] 1.0)
+                         (fn [i] 1.0)
+                         100))
+
+(defn d-euler [i]
+  (if (= (mod i 3) 2)
+    (* 2 (/ (+ i 1) 3))
+    1))
+
+(println (cont-frac-recur (fn [i] 1.0)
+                          d-euler
+                          100))
+
+(println (cont-frac-iter (fn [i] 1.0)
+                         d-euler
+                         100))
+
+(defn tan-cf [x k]
+  (cont-frac-iter
+   (fn [i] (if (= i 1) x (* x x -1)))
+   (fn [i] (- (* 2.0 i) 1))
+   k))
+
+(println (tan-cf 1 10))
+(println (math/tan 1))
--- src/structure_and_interpretation_of_computer_programs/math/fixed-point.clj
+++ src/structure_and_interpretation_of_computer_programs/math/fixed-point.clj
@@ -1,9 +1,9 @@
 (require '[clojure.math :as math])
 
-(def tolerance 0.00001)
+(def tolerance 0.000001)
 
 (defn close-enough? [v1 v2]
-  (< (Math/abs (- v1 v2)) tolerance))
+  (< (abs (- v1 v2)) tolerance))
 
 (defn fixed-point [f first-guess]
   (letfn [(try-fn [guess]
@@ -27,6 +27,50 @@
 (defn sqrt [x]
   (fixed-point  (fn [y] (average  [y (/ x y)]))   1.0))
 
+(defn golden-ratio []
+  (fixed-point  (fn [x] (+ 1 (/ 1 x))) 1.0))
+
+(defn average-damp [f]
+  (fn [x] (average [x (f x)])))
+
+(defn sqrt2 [x]
+  (fixed-point (average-damp (fn [y] (/ x y))) 1.0))
+
+(defn cube-root [x]
+  (fixed-point (average-damp (fn [y] (/ x (* y y)))) 1.0))
+
+(def dx 0.000001)
+
+(defn deriv [g]
+  (fn [x] (/ (- (g (+ x dx)) (g x)) dx)))
+
+(defn cube [x] (* x x x))
+
+(defn newton-transform [g]
+  (fn [x] (- x (/ (g x) ((deriv g) x)))))
+
+(defn newton-method [g guess]
+  (fixed-point (newton-transform g) guess))
+
+(defn sqrt3 [x]
+  (newton-method (fn [y] (- (* y y) x)) 1.0))
+
 (println (fixed-point math/cos 1.0))
 (println (fixed-point (fn [y] (+ (math/sin y) (math/cos y))) 1.0))
+(println "_______")
+
 (println (sqrt 2))
+(println (sqrt2 2))
+(println (sqrt3 2))
+(println "_______")
+
+(println (golden-ratio))
+(println "_______")
+
+(println (fixed-point (fn [x] (/ (math/log 1000) (math/log x))) 2))
+(println (fixed-point (fn [x] (average [x (/ (math/log 1000) (math/log x))])) 2))
+(println "_______")
+
+(println (cube-root 9))
+(println ((deriv cube) 5))
+(println "_______")
