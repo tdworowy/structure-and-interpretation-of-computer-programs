7a39c51935bcce1c8dbc81764aa8cfa31068386b
Tomasz Dworowy <dworowytomasz@gmail.com>
2024-12-21 11:11:47 +0100

    update

---
+++ src/structure_and_interpretation_of_computer_programs/exercises/chapter_2/fringe.clj
@@ -0,0 +1,8 @@
+(defn fringe [items]
+  (cond
+    (empty? items) []
+    (not (coll? (first items))) (cons (first items) (fringe (rest items)))
+    :else (concat (fringe (first items)) (fringe (rest items)))))
+
+
+(println (fringe [[1 2] [1 [3 4] 2] 3]))
---
+++ src/structure_and_interpretation_of_computer_programs/exercises/chapter_2/vectors-reverse.clj
@@ -0,0 +1,21 @@
+(defn reverse-vector [items]
+  (letfn [(reverse-vector-iter [vec new-vec n]
+            (if (neg? n)
+              new-vec
+              (reverse-vector-iter vec (conj new-vec (nth vec n)) (dec n))))]
+    (reverse-vector-iter items [] (dec (count items)))))
+
+(defn deep-reverse-vector [items]
+  (letfn [(iter-reverse [items]
+            (if (empty? items)
+              []
+              (let [first-item (first items)
+                    rest-reversed (iter-reverse (rest items))]
+                (if (vector? first-item)
+                  (conj rest-reversed (deep-reverse-vector first-item))
+                  (conj rest-reversed first-item)))))]
+    (iter-reverse items)))
+
+(println (reverse-vector [1 2 3 4]))
+(println (deep-reverse-vector [1 2 3 4]))
+(println (deep-reverse-vector [1 [2 3] 4]))
