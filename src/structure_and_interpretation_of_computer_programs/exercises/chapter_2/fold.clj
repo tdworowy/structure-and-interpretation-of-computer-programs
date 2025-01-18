0a1d66602e5d403293956347bf245d4baecfa6ae
Tomasz Dworowy <dworowytomasz@gmail.com>
2025-01-18 14:31:21 +0100

    update

--- src/structure_and_interpretation_of_computer_programs/exercises/chapter_2/fold.clj
+++ src/structure_and_interpretation_of_computer_programs/exercises/chapter_2/fold.clj
@@ -1,20 +1,25 @@
-(defn fold-left [op init sequence]
-  (letfn [(iter [result _rest]
-            (if (empty? _rest) result
-                (iter (op result (first _rest)) (rest _rest))))]
-    (iter init sequence)))
+;(defn fold-left [op init sequence]
+;  (letfn [(iter [result _rest]
+;            (if (empty? _rest) result
+;                (iter (op result (first _rest)) (rest _rest))))]
+;    (iter init sequence)))
 
-(defn fold-right [op init sequence]
-  (letfn [(iter [_rest result]
-            (if (empty? _rest)
-              result
-              (op (first _rest) (iter (rest _rest) result))))]
-    (iter sequence init)))
+(defn fold-left [op init sequence]
+  (if (empty? sequence)
+    init
+    (op (fold-left op init (rest sequence)) (first sequence))))
 
 ;(defn fold-right [op init sequence]
-;  (if (empty? sequence)
-;    init
-;    (op (first sequence) (fold-right op init (rest sequence)))))
+;  (letfn [(iter [_rest result]
+;            (if (empty? _rest)
+;              result
+;              (op (first _rest) (iter (rest _rest) result))))]
+;    (iter sequence init)))
+
+(defn fold-right [op init sequence]
+  (if (empty? sequence)
+    init
+    (op (first sequence) (fold-right op init (rest sequence)))))
 
 (println (fold-right / 1 [1 2 3]))
 (println (fold-left / 1 [1 2 3]))
