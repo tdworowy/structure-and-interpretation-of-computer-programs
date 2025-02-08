a416f89897cb3f1d8d38d22b5a2f23bef75d40d7
Tomasz Dworowy <dworowytomasz@gmail.com>
2025-02-08 11:33:19 +0100

    update

---
+++ src/structure_and_interpretation_of_computer_programs/abstraction_with_data/memq.clj
@@ -0,0 +1,8 @@
+(defn memq [item x]
+  (cond
+    (empty? x) false
+    (= item (first x)) x
+    :else (memq item (rest x))))
+
+(println (memq 'apple '(pear banana prune)))
+(println (memq 'apple '(x (apple sauce) y apple pear)))
---
+++ src/structure_and_interpretation_of_computer_programs/exercises/chapter_2/eight-queens-puzzle.clj
@@ -0,0 +1,18 @@
+
+(defn empty-board nil)
+(defn safe?) ;TODO
+(defn adjoin-position [new-row k rest-of-queens]
+  (cons rest-of-queens (list (list new-row k)))
+  ) ;TODO
+
+
+(defn queens [board-size]
+  (letfn [(queen-cols [k]
+            (if = k 0)
+            (list empty-board)
+            (filter (fn [positions] (safe? k positions))
+                    (mapcat (fn [rest-of-queens]
+                              (map (fn [new-row]
+                                     (adjoin-position new-row k rest-of-queens))
+                                   (queen-cols (- k 1)))))))]
+    (queen-cols board-size)))
